1a10046f01f086ae198cc7dd93bb1493736b5c47
BhawnaNAgra <bhawnagrajan01@gmail.com>
2022-11-10 20:25:18 +0530

    component-added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './header/recipes/recipes.component';
+import { RecipesDetailsComponent } from './header/recipes/recipes-details/recipes-details.component';
+import { RecipesListComponent } from './header/recipes/recipes-list/recipes-list.component';
+import { RecipesItemComponent } from './header/recipes/recipes-list/recipes-item/recipes-item.component';
+import { ShoppingListComponent } from './header/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './header/shopping-list/shopping-edit/shopping-edit.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipesDetailsComponent,
+    RecipesListComponent,
+    RecipesItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    
   ],
   imports: [
     BrowserModule,
